21290ba4cf30973fe17008182b29d63bacbbd6c7
Michael Bromley <michael@michaelbromley.co.uk>
2016-06-10 11:20:32 +0200

    Add template for unit tests file.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# cmpg (Component Generator)
+# cmpg (component generator)
 
 This is an ultra-simple cli tool for generating Angular2 component scaffolding, with naming conventions 
 taken from the [Angular 2 Style Guide](https://angular.io/styleguide)
@@ -11,16 +11,20 @@ Sass for styles. A good enhancement would be to somehow allow different template
 
 ### Usage
 
-This is not currently published on npm since I feel it is currently too trivial (too trivial for npm, you say?)..
+This is not currently published on npm since I feel it is currently too trivial (too trivial for npm, you say?).
+
+Requires Node v4+
 
 But it can be used like this:
 
 1. Clone this repo.
-2. `npm install -g [path just installed under]`
+2. `cd [repo dir]`
+3. `npm install -g .`
+4. Go to the new component location, e.g. `myapp/src/components/my-component`
 3. `cmpg my-component`
 
 
-That will generate a TypeScript, HTML and Sass file named according to
+That will generate a TypeScript component, test, HTML and Sass file named according to
 the argument passed:
 
 `> cmpg foo-selector`
@@ -30,6 +34,7 @@ result:
 ```
 ./
   |- foo-selector.component.ts
+  |- foo-selector.component.spec.ts
   |- foo-selector.component.html
   |- foo-selector.scss
 ```
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const mustache = require('mustache');
 const htmlTemplate = fs.readFileSync(path.join(__dirname, './templates/component-webpack/html.mustache'), 'utf8');
 const typescriptTemplate = fs.readFileSync(path.join(__dirname, './templates/component-webpack/typescript.mustache'), 'utf8');
 const sassTemplate = fs.readFileSync(path.join(__dirname, './templates/component-webpack/sass.mustache'), 'utf8');
+const testsTemplate = fs.readFileSync(path.join(__dirname, './templates/component-webpack/tests.mustache'), 'utf8');
 
 // ensure a component name argument has been specified.
 const componentName = process.argv[2];
@@ -22,9 +23,14 @@ const className = kebabToPascal(componentName) + 'Component';
 let context = { componentName, className };
 
 // output the component files.
-fs.writeFile(`${componentName}.component.ts`, mustache.render(typescriptTemplate, context));
-fs.writeFile(`${componentName}.component.html`, mustache.render(htmlTemplate, context));
-fs.writeFile(`${componentName}.scss`, mustache.render(sassTemplate, context));
+try {
+    fs.writeFile(`${componentName}.component.ts`, mustache.render(typescriptTemplate, context));
+    fs.writeFile(`${componentName}.component.html`, mustache.render(htmlTemplate, context));
+    fs.writeFile(`${componentName}.scss`, mustache.render(sassTemplate, context));
+    fs.writeFile(`${componentName}.component.spec.ts`, mustache.render(testsTemplate, context));
+} catch (e) {
+    console.log('An error occurred!', e);
+}
 
 console.log(`Created the ${componentName} component in the current directory.`);
 
